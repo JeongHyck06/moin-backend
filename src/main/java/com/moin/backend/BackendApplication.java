5ad365ff350d40059bf9c1ae03ebc3348c3245f4
Jack <devjack050700@gmail.com>
2026-09-16 22:20:33 +0900

    chore: 스케줄링 활성화

--- src/main/java/com/moin/backend/BackendApplication.java
+++ src/main/java/com/moin/backend/BackendApplication.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
@@ -15,6 +16,7 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 
 @SpringBootApplication
+@EnableScheduling
 @OpenAPIDefinition(info = @Info(title = "모인 API", version = "v1"), security = @SecurityRequirement(name = "bearer"))
 @SecurityScheme(name = "bearer", type = SecuritySchemeType.HTTP, scheme = "bearer", description = "POST /auth/dev 또는 /auth/kakao 응답의 token")
 public class BackendApplication {
